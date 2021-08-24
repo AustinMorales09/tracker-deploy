5990921ab2f7368280b30ec1ef94ff87da38159a
Austin <Harrenaustint@gmail.com>
2021-08-24 10:46:04 -0400

    [ush

---
+++ new.js
@@ -0,0 +1,10 @@
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+      .then(result => {
+        res.render('details', { blog: result, title: 'Blog Details' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
